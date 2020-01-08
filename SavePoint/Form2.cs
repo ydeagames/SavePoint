7bb92bd5cd065cde99ec6bfe502e52b7b5476c02
YdeaGames <ydeagames@gmail.com>
2020-01-08 22:01:52 +0900

    Setup

---
+++ SavePoint/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibGit2Sharp;
+
+namespace SavePoint
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Repository.Init(".");
+        }
+    }
+}
